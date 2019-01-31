0d4c18ebcea87e54ff3283199e432213214fb1cb
Thiago Thomaz <thiagorthomaz@gmail.com>
2019-01-31 15:52:01 -0200

    Added AdminLTE

--- CMS/App_Start/BundleConfig.cs
+++ CMS/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace CMS
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.1.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,12 +20,20 @@ namespace CMS
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/adminlte/js").Include(
+                      "~/admin-lte/js/adminlte.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/adminlte/css").Include(
+                      "~/admin-lte/css/AdminLTE.min.css",
+                      "~/admin-lte/css/skins/skin-blue.min.css"));
+
+
         }
     }
 }
---
+++ CMS/Controllers/PostsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Add View
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
